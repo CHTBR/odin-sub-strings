d66cb1b5f932008766b95003d945f8ffa97e84b5
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-07-03 10:14:30 +0200

    Split string into parts with length of substring

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,4 +1,7 @@
 def substrings (string, substr_dictionary)
+  string_chars = string.split("")
   substr_dictionary.reduce(Hash.new()) do |hash, substring|
+    substrings_of_length = []
+    string_chars.each_cons(substring.length) {|substr| substrings_of_length.push(substr.join(""))}
   end
 end
