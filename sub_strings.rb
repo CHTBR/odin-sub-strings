9778dcc0463c12345e76c2bfc50b76d70294328f
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-07-03 10:19:45 +0200

    Count number of matches of substrings

--- sub_strings.rb
+++ sub_strings.rb
@@ -3,5 +3,8 @@ def substrings (string, substr_dictionary)
   substr_dictionary.reduce(Hash.new()) do |hash, substring|
     substrings_of_length = []
     string_chars.each_cons(substring.length) {|substr| substrings_of_length.push(substr.join(""))}
+    num_of_occurences = substrings_of_length.count(substring)
+    hash[substring] = num_of_occurences if num_of_occurences > 0
+    hash
   end
 end
